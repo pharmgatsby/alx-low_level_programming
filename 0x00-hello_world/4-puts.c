9cdffdd2b44839edaa3f90ea2e1f8a40e442a257
pharmgatsby <pharmgatsby981@gmail.com>
2022-09-08 11:35:23 -0700

    sef

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  * Return: always 0
  */
 int main(void)
-{  puts("\"Programming is like building a multilingual puzzle");
+{ puts("\"Programming is like building a multilingual puzzle");
  return (0);
 }
