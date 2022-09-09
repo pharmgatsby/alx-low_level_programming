71eda6dd1684337ceb8b75b57ca64bc7537cc0cb
pharmgatsby <pharmgatsby981@gmail.com>
2022-09-09 14:01:28 -0700

    spell

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 lastn = n % 10;
 if (lastn > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, last);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 }
 else if (lastn == 0)
 {
