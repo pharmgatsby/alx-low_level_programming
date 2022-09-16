9c5ca65e8ebbf43b448ffe8feabc80dc5ccb49e3
pharmgatsby <pharmgatsby981@gmail.com>
2022-09-16 13:12:45 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,12 @@
  *Return: 1 whether it is, 0 otherwise
  */
 
-int _isupper(int c)
+int _isupper(int c) 
 {
   if ((c >= 'A') && (c <= 'Z'))
     {
-      return (1);    
+      return (1);      
     }
-  
-  return (0);  
+
+  return (0); 
 }
