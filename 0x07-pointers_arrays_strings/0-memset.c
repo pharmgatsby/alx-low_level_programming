85ffa68a891f992de4ee87474695cfc8d4f725b0
pharmgatsby <pharmgatsby981@gmail.com>
2022-09-26 13:44:42 -0700

    stronghold

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
-*_memset - fills a memory block with a constant byte
-*@s: address to memory block
-*@b: char to be used 
-*
-*Return: pointer to the memory block
-*/
-
-char *_memset(char *s, char b, unsigned int n)
+ * _memset - fills memory with a constant byte
+ * @s: memory space to be filled
+ * @b: byte to fill with
+ * @n: number of spaces to fill
+ *
+ * Return: pointer to memory area s
+ */
+char *_memset(char *s, char b, unsigned int n) 
 {
-while (n)
-  {
-s[n - 1] = b;
-n ==;
-  }
-return (s);
+  unsigned int i;
+  
+  for (i = 0; i < n; i++)
+    {
+      *(s + i) = b; 
+    }
+  return (s); 
 }
